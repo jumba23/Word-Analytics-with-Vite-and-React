2d9c68589653b63360dad817b4b8298ba059380c
Goran SD <g.cvetic23@gmail.com>
2024-03-05 10:33:17 -0800

    Refactor TextArea component to use useState hook

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const TextArea = () => {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+
+  return (
+    <textarea
+      className="textarea"
+      value={text}
+      onChange={(e) => {
+        const newText = e.target.value;
+        setText(newText);
+      }}
+      placeholder="Enter your text"
+      spellCheck="false"
+    />
+  );
 };
 
 export default TextArea;
