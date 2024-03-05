8c56867526c58d75cb22d7f05612f3572f1d81bd
Goran SD <g.cvetic23@gmail.com>
2024-03-05 10:15:30 -0800

    Add footer class and create H1 component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer>
+    <footer className="footer">
       <small>&copy; Copyright by SoftSolutions. All rights reserved.</small>
       <small>Last checked limits: 4 minutes ago.</small>
     </footer>
