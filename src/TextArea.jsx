0119d3edfc159e39609dbc46bac79e387c1b649d
Goran SD <g.cvetic23@gmail.com>
2024-03-05 10:40:39 -0800

    Fix script tag vulnerability in TextArea component

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -3,14 +3,20 @@ import { useState } from "react";
 const TextArea = () => {
   const [text, setText] = useState("");
 
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      alert("No script tag allowed");
+      newText = newText.replace("<script>", "");
+    }
+    setText(newText);
+  };
+
   return (
     <textarea
       className="textarea"
       value={text}
-      onChange={(e) => {
-        const newText = e.target.value;
-        setText(newText);
-      }}
+      onChange={handleChange}
       placeholder="Enter your text"
       spellCheck="false"
     />
