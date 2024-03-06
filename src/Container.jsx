df693f321eb9c0b79d92ed7a26c4f018c52019bf
Goran SD <g.cvetic23@gmail.com>
2024-03-06 09:51:19 -0800

    Add constants for Instagram and Facebook character limits

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./Constants";
 
 const Container = () => {
   const [text, setText] = useState("");
 
   const stats = {
     numberOfCharacters: text.length,
-    instagramCharactersLeft: 280 - text.length,
-    facebookCharactersLeft: 2200 - text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
     numberOfWords: text.split(" ").filter((word) => word !== "").length,
   };
 
