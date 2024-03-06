0bb4c2b68d3990eb7b9746d18a69567b90d9c36a
Goran SD <g.cvetic23@gmail.com>
2024-03-05 23:03:17 -0800

    Add character count and word count stats to Container and Stats components

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,13 +4,20 @@ import TextArea from "./TextArea";
 
 const Container = () => {
   const [text, setText] = useState("");
-
+  const numberOfWords = text.split(" ").filter((word) => word !== "").length;
   const numberOfCharacters = text.length;
+  const instagramCharactersLeft = 280 - numberOfCharacters;
+  const facebookCharactersLeft = 2200 - numberOfCharacters;
 
   return (
     <main className="container">
       <TextArea text={text} setText={setText} />
-      <Stats numberOfCharacters={numberOfCharacters} />
+      <Stats
+        numberOfCharacters={numberOfCharacters}
+        instagramCharactersLeft={instagramCharactersLeft}
+        facebookCharactersLeft={facebookCharactersLeft}
+        numberOfWords={numberOfWords}
+      />
     </main>
   );
 };
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,10 +1,15 @@
-const Stats = ({ numberOfCharacters }) => {
+const Stats = ({
+  numberOfCharacters,
+  instagramCharactersLeft,
+  facebookCharactersLeft,
+  numberOfWords,
+}) => {
   return (
     <section className="stats">
-      <Stat number={0} label="Words" />
+      <Stat number={numberOfWords} label="Words" />
       <Stat number={numberOfCharacters} label="Characters" />
-      <Stat number={280} label="Instagram " />
-      <Stat number={2200} label="Facebook" />
+      <Stat number={instagramCharactersLeft} label="Instagram " />
+      <Stat number={facebookCharactersLeft} label="Facebook" />
     </section>
   );
 };
